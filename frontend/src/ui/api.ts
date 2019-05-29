8592d9d880f65d5f629758f86993c447b71f01de
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-05-29 19:08:12 +0200

    Minor styling tweaks

--- frontend/src/ui/api.ts
+++ frontend/src/ui/api.ts
@@ -17,7 +17,9 @@ export async function getChallenges (): Promise<ApiChallenge[]> {
     {
       id: 0,
       title: 'Introduction',
-      description: 'Write a function that returns true'
+      description: 'Write a function that returns true.\n' +
+        'Multiline description is something very important and must be ' +
+        'supported in order to allow for optimal experience.'
     },
     {
       id: 1,
