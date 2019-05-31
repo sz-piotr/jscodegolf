95a6440776a6579bc83be1b5069cac3050346e78
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-05-31 18:13:18 +0200

    Add paths and body

--- backend/src/util/validate.ts
+++ backend/src/util/validate.ts
@@ -12,34 +12,58 @@ function isRight<R> (value: Either<any, R>): value is { right: R } {
 
 type Error = { path: string, expected: string }
 
-type Sanitize<T> = (data: any) => Either<T, Error[]>
+type Sanitize<T> = (data: unknown, path: string) => Either<T, Error[]>
 
 type Schema<T> = {
   [K in keyof T]: Sanitize<T[K]>
 }
 
 export const sanitize = <T>(schema: Schema<T>) => (req: Request) => {
-  let res: T = {} as any
-
+  const sanitized: T = {} as any
   for (const key in schema) {
-    const result = schema[key](req.params[key])
+    let result
+    if (key === 'body') {
+      result = schema[key](req.body, 'body')
+    } else {
+      result = schema[key](req.params[key], `params.${key}`)
+    }
     if (isLeft(result)) {
-      res[key] = result.left
+      sanitized[key] = result.left
     } else {
       throw result.right // TODO: figure it out
     }
   }
-
-  return res
+  return sanitized
 }
 
-export const asString: Sanitize<string> = (data: unknown) => {
+export const asObject = <T extends object> (schema: Schema<T>): Sanitize<T> =>
+  function (data, path) {
+    if (typeof data !== 'object' || data === null) {
+      return right([{ path: '', expected: 'object' }])
+    }
+    const value: T = {} as any
+    const errors: Error[] = []
+    for (const key in schema) {
+      const result = schema[key]((data as T)[key], `${path}.${key}`)
+      if (isLeft(result)) {
+        value[key] = result.left
+      } else {
+        errors.push(...result.right)
+      }
+    }
+    if (errors.length > 0) {
+      return right(errors)
+    }
+    return left(value)
+  }
+
+export const asString: Sanitize<string> = (data, path) => {
   if (typeof data === 'string') return left(data)
-  else return right([{ path: '', expected: 'string' }])
+  else return right([{ path, expected: 'string' }])
 }
 
-export const asNumber: Sanitize<number> = (data: unknown) => {
+export const asNumber: Sanitize<number> = (data, path) => {
   const val = +(data as any)
   if (!isNaN(val)) return left(val)
-  else return right([{ path: '', expected: 'number' }])
+  else return right([{ path, expected: 'number' }])
 }
