b5cf65ad9c3ab77bf1674b3e9ace02801e44f85c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-06-15 12:08:02 +0200

    Add a working worker

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,8 +5,17 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import { App } from './ui/App'
+import MyWorker from 'worker-loader!./worker'
 
 render(
   <App />,
   document.getElementById('app'),
 )
+
+
+const worker = new MyWorker();
+
+worker.postMessage({ a: 1 });
+worker.onmessage = (event) => { console.log(1, event) };
+
+worker.addEventListener("message", (event) => { console.log(2, event) });
