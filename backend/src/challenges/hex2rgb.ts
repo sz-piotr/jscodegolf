b634a539ab84865fc6f29224374302103bd27030
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-06-16 16:37:11 +0200

    Add hex2rgb

---
+++ backend/src/challenges/hex2rgb.ts
@@ -0,0 +1,33 @@
+import { Challenge } from './Challenge'
+
+export const hex2rgb: Challenge = {
+  id: 'hex2rgb',
+  title: 'HEX 2 RGB',
+  description: 'Write a function that transforms hex encoded colors to rgb ' +
+    'encoded colors. It should support both #FFF and #FFFFFF.',
+  tests: [
+    ['#B04B4B', 'rgb(176,75,75)'],
+    ['#7E88E6', 'rgb(126,136,230)'],
+    ['#1FA11F', 'rgb(31,161,31)'],
+    ['#123', 'rgb(17,34,51)'],
+    ['#7FFFD4', 'rgb(127,255,212)'],
+    ['#B0E0E6', 'rgb(176,224,230)'],
+    ['#5F9EA0', 'rgb(95,158,160)'],
+    ['#4682B4', 'rgb(70,130,180)'],
+    ['#6495ED', 'rgb(100,149,237)'],
+    ['#00BFFF', 'rgb(0,191,255)'],
+    ['#1E90FF', 'rgb(30,144,255)'],
+    ['#ADD8E6', 'rgb(173,216,230)'],
+    ['#87CEEB', 'rgb(135,206,235)'],
+    ['#87CEFA', 'rgb(135,206,250)'],
+    ['#666', 'rgb(102,102,102)'],
+    ['#F00', 'rgb(255,0,0)'],
+  ].map(
+    ([a, b]) => ({ type: 'FUNCTION', args: [a], expected: b })
+  )
+}
+
+
+/*
+assertType(result, 'function');
+*/
--- backend/src/challenges/index.ts
+++ backend/src/challenges/index.ts
@@ -2,9 +2,11 @@ import { Challenge } from './Challenge'
 import { introduction } from './introduction'
 import { divisors } from './divisors'
 import { jscodegolf } from './jscodegolf'
+import { hex2rgb } from './hex2rgb'
 
 export const challenges: Challenge[] = [
   introduction,
   divisors,
   jscodegolf,
+  hex2rgb,
 ]
